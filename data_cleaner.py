96b81c0bdaa0c6846ca9a24e767991b9db37f60a
rmagala <rmagala640@gmail.com>
2025-02-09 14:54:02 -0600

    optimization and data cleaning

--- data_cleaner.py
+++ data_cleaner.py
@@ -6,7 +6,7 @@ author: <NAME> Richard Magala
 email; magalarich20@gmail.com
 """
 from typing import Union
-
+from xlwings import view
 import pandas as pd
 from numpy import ndarray
 from os.path import (join, dirname, realpath)
@@ -19,7 +19,6 @@ fileName = join(baseDir, 'ghgData.csv')
 ExpectedColumns = {'to_carb', 'soil_type', 'ch4_kg_ha_yr', 'TopN2O', 'kpi',
                    'Whole_repsiration', 'precipitation_level', 'land_use',
                    'land_use_code'}
-
 landUSes = {'Conservation forest',
             'Conventional forest',
             'Mixed fruits and vegetables',
@@ -72,15 +71,14 @@ def _check_values(values: Union[ndarray, list, tuple], category: str) -> None:
         raise ValueError(f"{category} is missing some treatments levels {dif}")
 
 
-def view(dat_v):
-    from xlwings import view
-    view(dat_v, table=False)
-
-
-def load_and_clean(view_in_excel: bool = False) -> pd.DataFrame:
+def load_and_clean(data =None, view_in_excel: bool = False, **kwargs) -> pd.DataFrame:
     """"checks for duplicates in the data to send to the serve"""
-
-    df = pd.read_csv('kpi.csv')
+    file_name = kwargs.get('file_name', fileName)
+    path = kwargs.get('path', 'kpi.csv')
+    if data is not None:
+        df = data.copy()
+    else:
+        df = pd.read_csv(path)
     # check if all columns are in the data frame
     interSec = df.columns.intersection(ExpectedColumns)
     Tru = [x == y for x, y in zip(interSec, ExpectedColumns)]
@@ -94,16 +92,27 @@ def load_and_clean(view_in_excel: bool = False) -> pd.DataFrame:
     if 'Unnamed: 0' in df.columns:
         df.drop('Unnamed: 0', axis=1, inplace=True)
     data = df.drop_duplicates(subset=['land_use_code', 'precipitation_level', 'soil_type'])
-    # check precipitation
-    _check_values(data.precipitation_level, category='precipitation')
-    # check land use codes
+
     _check_values(data.land_use_code, category='land_use')
-    # check soil types
-    _check_values(data.soil_type, category='soil')
+    land_use_codes = data.land_use_code.unique()
+
+    def query_data(code):
+        return data[data['land_use_code'] == code].copy()
+
+    # each land use should have all the treatment
+    for code_ in land_use_codes:
+        print(code_)
+        cdata = query_data(code_)
+        # check precipitation
+        _check_values(cdata.precipitation_level, category='precipitation')
+        # check land use codes
+
+        # check soil types
+        _check_values(cdata.soil_type, category='soil')
     data.reset_index(drop=True, inplace=True)
-    data.to_csv(fileName, index=False)
+    data.to_csv( file_name, index=False)
     if view_in_excel:
-        view(data)
+        view(data, table=kwargs.get('table', True))
     return data
 
 
