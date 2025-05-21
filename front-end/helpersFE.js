fb4e5947e15dcb35876973f5551fe5753866890e
vanguard <rmagala640@gmail.com>
2025-05-20 21:38:26 -0500

    randomize using ctrl + b

--- front-end/helpersFE.js
+++ front-end/helpersFE.js
@@ -7435,6 +7435,12 @@ function printOptionsEsc(e) {
   }
 } // end printOptionsEsc
 
+const RandomizeLandUse = () => {
+  // randomize_landuses
+  const landIDS = [1,2,3,4,5,6,7,8,9,10,11,12,12,14,15]
+};
+
+
 //Calculate Game Wildlife score for an individual tile
 function getTileGameWildlifeScore(tileId){
   var score = 0;
